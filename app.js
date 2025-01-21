749a46b21d32a1f4a4d53d49ff16e73f5c437f6e
YuriSantuche <yurisantuche@gmail.com>
2025-01-21 15:27:33 -0300

    alterando o limite para 100 numeros

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -12,7 +12,7 @@ function alterarTexto(tag, texto){
 exibirMensagemIncial();
 function exibirMensagemIncial(){
     alterarTexto('h1','Jogo do número secreto.');
-    alterarTexto('p','Escolha um número entre 1 e 10.')
+    alterarTexto('p','Escolha um número entre 1 e 100.')
 }
 
 function verificarChute(){
